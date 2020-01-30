d885106f2f8e697fc26406edc80bb24d1402013e
Alice Won <alicewon4@gmail.com>
2020-01-30 09:42:26 -0800

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -19,6 +19,19 @@ class Timer extends Component {
     );
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    //takes in two arguments, the next props and state from the potential update. 
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
+  componentDidUpdate() {
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
   componentWillUnmount() {
     clearInterval(this.interval);
   }
